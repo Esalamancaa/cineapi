f9f33ff657c793f3cb3e89c4f86d20571027b936
Esalamancaa <esteban.salamanca.aravena@gmail.com>
2026-04-27 12:09:30 -0400

    Actualización Repository

--- cineapi/src/main/java/cl/cine/cineapi/repository/PeliculaRepository.java
+++ cineapi/src/main/java/cl/cine/cineapi/repository/PeliculaRepository.java
@@ -1,7 +1,21 @@
 package cl.cine.cineapi.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import cl.cine.cineapi.model.Pelicula;
 
-public class PeliculaRepository {
+@Repository
+public interface PeliculaRepository extends JpaRepository<Pelicula, Integer>{
 
+    default int totalPeliculas(){
+        return (int) this.count();
+    }
+
+    @Query(value = "SELECT * FROM libros WHERE generoPelicula = :genero", nativeQuery = true)
+    List <Pelicula> findByGenero(@Param("genero") String genero);
 }
